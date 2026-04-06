fa389904d7a4d51c3747d3c72d80aa124cbd26ed
Ankita Sarkar <ankitasarkar.sectionn@gmail.com>
2026-04-06 19:54:01 +0530

    Implement Kruskal's algorithm for minimum spanning tree

--- All topics/prims_krushkal.c
+++ All topics/prims_krushkal.c
@@ -33,6 +33,43 @@ void prims() {
   }
   printf("Total Cost: %d", total);
 }
+
+int parent[MAX];
+int find(int i) {
+  while(parent[i]!=i)
+    i=parent[i];
+  return i;
+}
+void unionSet(int i, int j) {
+  int a=find(i);
+  int b=find(j);
+  parent[a]=b;
+}
+void krushkal() {
+  int total=0, ne=0;
+  for(int i=0;i<n;i++)  parent[i]=i;
+
+  while(ne<n-1) {
+    int min=INF, x=0, y=0;
+    for(int i=0;i<n;i++) {
+      for(int j=0;j<n;j++) {
+        if(find(i)!=find(j) && graph[i][j]) {
+          if(graph[i][j]<min) {
+            min=graph[i][j];
+            x=i;
+            y=j;
+          }
+        }
+      }
+    }
+    unionSet(x,y);
+    printf("Edge (%d-%d) Cost: %d\n", x, y, min);
+    total+=min;
+    ne++;
+  }
+  printf("Total Cost: %d", total);
+}
+
 int main() {
     printf("Enter number of vertices: ");
     scanf("%d", &n);
@@ -45,6 +82,7 @@ int main() {
     }
 
     prims();
+    krushkal();
 
     return 0;
 }
